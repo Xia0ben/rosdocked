b4578e3b70229dce7d67cfdbe92ebababa1ea31c
Xia0ben <benoit.renault@insa-lyon.fr>
2018-05-25 18:30:20 +0200

    Add sudo to docker command and --name so that it's easier to find the container

--- run.sh
+++ run.sh
@@ -14,11 +14,13 @@ popd > /dev/null
 set -e
 
 # Run the container with shared X11
-docker run\
+sudo docker run\
   --net=host\
   -e SHELL\
   -e DISPLAY\
   -e DOCKER=1\
   -v "$HOME:$HOME:rw"\
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
+  --name $1\
   -it $1 $SHELL
+
